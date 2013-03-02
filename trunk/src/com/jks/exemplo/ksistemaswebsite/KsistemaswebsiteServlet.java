535c007cb44ddccb3ac99c9b4720e3ee613cca0d
Kim Prado <kimprado21@gmail.com>
2013-03-02 16:26:27 -0300

    Criação do teste.

---
+++ trunk/src/com/jks/exemplo/ksistemaswebsite/KsistemaswebsiteServlet.java
@@ -0,0 +1,13 @@
+package com.jks.exemplo.ksistemaswebsite;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class KsistemaswebsiteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
