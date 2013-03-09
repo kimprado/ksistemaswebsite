f1e4bff212cec69b1fae0b46ee84921d9bd50832
Kim Prado <kimprado21@gmail.com>
2013-03-09 00:58:38 -0300

    Alterar para sessio scope

--- trunk/src/com/jks/exemplo/ksistemaswebsite/mb/ClienteBean.java
+++ trunk/src/com/jks/exemplo/ksistemaswebsite/mb/ClienteBean.java
@@ -3,14 +3,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import com.jks.exemplo.ksistemaswebsite.negocio.Cliente;
 import com.jks.exemplo.ksistemaswebsite.negocio.Dependente;
 
 @ManagedBean(name="clienteMB")
-@ApplicationScoped
+@SessionScoped
 public class ClienteBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
